277f1312dc69c1bf150bfe0592bac3bc2d031b22
Aftab Mumtaz <aftabmumtaz14@gmail.com>
2025-11-10 11:46:37 +0500

    AftabStack

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home Page', message: 'Welcome to the Home Page!' });
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
